b8b22cb1b4e9aebf2572743f2def573b2ccae09b
Tonsofnamesz <Jonathanryan2015@gmail.com>
2023-11-27 12:47:59 +0700

    DAR

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import CartIcon from './pics/Thumbnails/cart.png';
+import SearchIcon from './pics/Thumbnails/Search.png';
+import logo from './pics/Thumbnails/placeholder.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="logo">LOGO HERE</div>
+        <div className="nav">
+          <button className="signup">Sign Up/Register</button>
+          <div className="search-container">
+            <div className="search-bar">
+              <input type="text" placeholder="Search products..." />
+              <button className="search-button">
+                <img src={SearchIcon} alt="Search" />
+              </button>
+            </div>
+          </div>
+          <div className="cart-container">
+            <button className="cart-button">
+              <img src={CartIcon} alt="Cart" />
+              <span className="cart-text">Cart</span>
+            </button>
+          </div>
+        </div>
       </header>
+      <div className="content">
+        <section className="background-section">
+          <div className="background-left"></div>
+          <div className="background-middle">
+            <img src={logo} alt="WIP Big Logo" />
+          </div>
+          <div className="background-right"></div>
+        </section>
+      </div>
     </div>
   );
 }
+
 
 export default App;
