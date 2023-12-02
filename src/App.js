add6f3ba75a3d65fcb8dc4c5ab3b586246d354ab
Tonsofnamesz <Jonathanryan2015@gmail.com>
2023-12-02 10:03:26 +0700

    SUDar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect, useState} from "react";
 import './App.css';
 import CartIcon from './pics/Thumbnails/cart.png';
 import SearchIcon from './pics/Thumbnails/Search.png';
@@ -6,6 +6,26 @@ import logo from './pics/Thumbnails/LogoWhole.png';
 import logosmall from './pics/Thumbnails/LogoSmall.png';
 
 function App() {
+
+  const [previewFeutItems, setPreviewFeutItems] = useState([]);
+  const [previewReccItems, setPreviewReccItems] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((data) => {
+      const shuffledItems = data.sort(() => 0.5 - Math.random());
+      const previewFeutItems = shuffledItems.slice(0, 5);
+      const previewReccItems = shuffledItems.slice(5, 10); 
+      setPreviewFeutItems(previewFeutItems);
+      setPreviewReccItems(previewReccItems)
+    });
+  };
+
   return (
     <div className="App">
       <div className="Stuffs">
@@ -43,16 +63,56 @@ function App() {
         </div>
       </div>
       <div className="content">
-        <section className="background-section">
+        <div className="background-section">
           <div className="background-left"></div>
           <div className="background-middle">
             <img src={logo} alt="Big_Logo" style={{ width: '350px', height: 'auto' }}/>
           </div>
           <div className="background-right"></div>
-        </section>
+        </div>
+        <div className="featured">Featured</div>
+        <div className="itemCardContainer">
+          {previewFeutItems.map((item) => (
+              <div key={item.id} className="itemCard">
+                <img src={item.image} alt={item.title} />
+                <p>{item.title}</p>
+                <p>Price: ${item.price}</p>
+              </div>
+            ))}
+            <div className="showAllButton">
+              <button>All</button>
+            </div>
+        </div>
+        <div className="Recommended">Recommended For You!</div>
+        <div className="itemCardContainer">
+          {previewReccItems.map((item) => (
+              <div key={item.id} className="itemCard">
+                <img src={item.image} alt={item.title} />
+                <p>{item.title}</p>
+                <p>Price: ${item.price}</p>
+              </div>
+            ))}
+            <div className="showAllButton">
+              <button>All</button>
+            </div>
+          <div className="AllPreview">All</div>
+          <div className="itemCardContainer">
+          {previewReccItems.map((item) => (
+              <div key={item.id} className="itemCard">
+                <img src={item.image} alt={item.title} />
+                <p>{item.title}</p>
+                <p>Price: ${item.price}</p>
+              </div>
+            ))}
+            <div className="showAllButton">
+              <button>All</button>
+            </div>
+        </div>
       </div>
+    </div>
     </div>
   );
 }
+
 
 export default App;
