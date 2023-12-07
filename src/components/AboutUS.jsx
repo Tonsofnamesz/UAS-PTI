43e6bb9cf10c22c3140d39560a4c7d5f781f5753
Tonsofnamesz <Jonathanryan2015@gmail.com>
2023-12-07 23:29:48 +0700

    AboutUs Added

--- src/components/AboutUS.jsx
+++ src/components/AboutUS.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import '../AboutUs.css';
+import RyanPic from '../pics/Thumbnails/The Unknown.jpg';
 
 const AUS  = () => {
     return (
         <div className="AboutUS">
-            <h1>INSERT SOMETHING</h1>
+            <h1>About Us</h1>
+            <div className="aboutUsContainer">
+                <img src={RyanPic} className="aboutUsImage" />
+                <div className="aboutUsInfo">
+                    <h2 className="aboutUsName">Jonathan Ryan R.</h2>
+                    <p className="aboutUsDescription">Tukang Sabun</p>
+                    <p className="aboutUsQuote">PIPEBOMB</p>
+                </div>
+            </div>
+            <div className="aboutUsContainer">
+                <img src={""} className="aboutUsImage" />
+                <div className="aboutUsInfo">
+                    <h2 className="aboutUsName">Matthew Sebastian Kusnandar</h2>
+                    <p className="aboutUsDescription">Tukang Sabun</p>
+                    <p className="aboutUsQuote">PIPEBOMB</p>
+                </div>
+            </div>
         </div>
     )
 }
