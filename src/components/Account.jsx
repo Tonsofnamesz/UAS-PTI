e3c33d3028ec3bb5d0c999307e54c43629423ae8
Tonsofnamesz <Jonathanryan2015@gmail.com>
2023-12-10 12:38:44 +0700

    Momentary Account

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import profilePic from '../pics/Thumbnails/blank-profile-pic.png'; // Replace with the actual path to your profile picture
+import profilePic from '../pics/Thumbnails/blank-profile-pic.png';
 import CartIcon from '../pics/Thumbnails/cart.png';
 import '../Account.css';
 import Header from '../Header';
@@ -8,75 +8,27 @@ import Footer from '../Footer';
 
 
 const Account = () => {
-    const [wishlistVisible, setWishlistVisible] = useState(false);
-    const [balanceVisible, setBalanceVisible] = useState(false);
-    const [wishlistItems, setWishlistItems] = useState([""]);
-    const [userBalance, setUserBalance] = useState("");
-  
-    const handleWishlistClick = () => {
-      setWishlistVisible(!wishlistVisible);
-      setBalanceVisible(false); // Close the balance dropdown if open
-    };
-  
-    const handleBalanceClick = () => {
-      setBalanceVisible(!balanceVisible);
-      setWishlistVisible(false); // Close the wishlist dropdown if open
-    };
-  
-    const mainContainerStyle = {
-      height: wishlistVisible || balanceVisible ? "300px" : "150px", // Adjust the heights as needed
-    };
-  
     return (
         <div>
-      <div className="Account">
-         <div className="headerContainer">
-          <Header />
-        </div>
-      <div className="secondHeaderContainer">
-          <Navigation />
-        </div>
-        <div className="account-container" style={mainContainerStyle}>
-          <div className="user-profile">
-            <img src={profilePic} alt="Profile" />
-            <span className="user-name">User's Name</span>
-          </div>
-          <div className="buttons-container">
-            <button className="account-button" onClick={() => console.log("My Cart clicked")}>
-              <img src={CartIcon} alt="Cart" />
-              My Cart
-            </button>
-            <button className="account-button" onClick={handleWishlistClick}>
-              Wishlist
-            </button>
-            <button className="account-button" onClick={handleBalanceClick}>
-              My Balance
-            </button>
-          </div>
-          {wishlistVisible && (
-            <div className="wishlist-dropdown">
-              <ul>
-                {wishlistItems.map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </ul>
+          <div className="Account">
+            <div className="headerContainer">
+              <Header />
             </div>
-          )}
-          {balanceVisible && (
-            <div className="balance-dropdown">
-              <p>Your Balance:</p>
-              <input
-                type="text"
-                placeholder="Enter your balance"
-                value={userBalance}
-                onChange={(e) => setUserBalance(e.target.value)}
-                style={{ border: "1px solid #ccc", padding: "5px" }}
-              />
+            <div className="secondHeaderContainer">
+              <Navigation />
             </div>
-          )}
-        </div>
-      </div>
-      <Footer />
+            <div className="ProfileContainerTopLeft">
+              <img src={profilePic} className="pfp" />
+              <h1>user'sn name from localstorage</h1>
+            </div>
+            <div className="NavbarBelowProfileContainerLeft">
+
+            </div>
+            <div className="NavContentsContainerRight">
+
+            </div>
+          </div>
+          <Footer />
         </div>
     );
   };
