c2d6151f4617a7c5734c67a622ffb001392ed556
D4rrylPrt <112367562+Seoren-343@users.noreply.github.com>
2023-12-11 11:44:55 +0700

    massive changes in login page

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Footer.css';
+import Element1 from './pics/Background_Imgs/element1footer.png';
 
 const Footer = () => (
-  <div style={{marginTop: '250px'}}>
-  <footer>
+  <div className="FooterContainer">
+    <div className="Element4">
+      <img src={Element1}></img>
+    </div>
+    <footer>
   <div>
     <h2>Our Company</h2>
     <ul>
@@ -12,23 +17,23 @@ const Footer = () => (
       <li><Link to="/privacy-policies">Privacy Policies</Link></li>
     </ul>
   </div>
-  <div>
-    <h2>Support</h2>
-    <ul>
-      <li><a href="/payment-terms">Payment Terms</a></li>
-      <li><Link to="/return-refund-policies">Return & Refund Policies</Link></li>
-      <li><a href="/contact-us">Contact Us</a></li>
-      <li><a href="/customer-help">Customer Help</a></li>
-    </ul>
-  </div>
-  <div>
-    <h2>Contact</h2>
-    <p>123 Main Street, Cityville, Country</p>
-  </div>
-</footer>
-<div className="copyright">
-        <p>2023 Invenire Griffon. All rights reserved.</p>
-      </div>
+    <div>
+      <h2>Support</h2>
+      <ul>
+        <li><a href="/payment-terms">Payment Terms</a></li>
+        <li><Link to="/return-refund-policies">Return & Refund Policies</Link></li>
+        <li><a href="/contact-us">Contact Us</a></li>
+        <li><a href="/customer-help">Customer Help</a></li>
+      </ul>
+    </div>
+    <div>
+      <h2>Contact</h2>
+      <p>123 Main Street, Cityville, Country</p>
+    </div>
+  </footer>
+  <div className="copyright">
+      <p>2023 Invenire Griffon. All rights reserved.</p>
+    </div>
   </div>
 
 );
--- src/components/LoginSignup.jsx
+++ src/components/LoginSignup.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../LoginSignup.css';
+import Element1 from '../pics/Background_Imgs/element1true.png';
+import LoginVecUp from '../pics/Background_Imgs/loginpagevectorup-removebg-preview.png';
+import LoginVecDown from '../pics/Background_Imgs/loginpagevectordown-removebg-preview.png';
+import Element5 from '../pics/Background_Imgs/element5.png';
+import Element2 from '../pics/Background_Imgs/element1footer.png';
 import userIcon from '../pics/Thumbnails/person.png';
 import emailIcon from '../pics/Thumbnails/email.png';
 import passwordIcon from '../pics/Thumbnails/password.png';
+import Footer from '../Footer';
 
 const LoginSignup = () => {
   const navigate = useNavigate();
@@ -11,16 +17,10 @@ const LoginSignup = () => {
   const [name, setName] = useState('');
 
   const handleSignup = () => {
-    // Save the name to localStorage
     localStorage.setItem('savedName', name);
-
-    // Perform signup logic here
-
-    // Redirect to the Account page
     navigate('/');
   };
 
-  // Load savedName from localStorage on component mount
   React.useEffect(() => {
     const savedName = localStorage.getItem('savedName');
     if (savedName) {
@@ -30,7 +30,19 @@ const LoginSignup = () => {
 
   return (
     <div className="LoginSign">
+      <div className="WebTitle">INVENIRE GRIFFON</div>
       <div className="TopSpacing"></div>
+      <div className="Element1">
+        <img src={Element1}></img>
+      </div>
+      <div className="LoginVectorUp">
+        <img src={LoginVecUp}></img>
+      </div>
+      <div className="Element5">
+        <img src={Element5}></img>
+      </div>
+      <div className="WebTitle2">INVENIRE</div>
+      <div className="WebTitle3">GRIFFON</div>
       <div className="Logincontainer">
         <div className="header">
           <div className="text">{action}</div>
@@ -65,7 +77,7 @@ const LoginSignup = () => {
         </div>
         {action !== 'Sign Up' && (
           <div className="forgot-password">
-            Forget Password?<span>Click Here!</span>
+            Forget Password?<span> Click Here!</span>
           </div>
         )}
         <div className="submit-container">
@@ -76,8 +88,13 @@ const LoginSignup = () => {
             Login
           </div>
         </div>
+        <div className="LoginVectorDown">
+          <img src={LoginVecDown}></img>
+        </div>
       </div>
-    </div>
+      <div style={{ marginTop: '150px' }}></div>
+      <Footer />
+      </div>
   );
 };
 
